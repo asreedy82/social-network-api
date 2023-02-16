1462986fb749e5a76b093b0dac3bf4820e4f6ff2
areedy <a.reedy@gmail.com>
2023-02-15 22:43:46 -0500

    removing utils as it is not needed

--- utils/helpers.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    // takes timestamp
-    format_date: (date) => {
-        //converts to month + date + year
-        return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${
-            new Date(date).getFullYear()
-            }`;
-    },
-};
